55893edeff555d602f1c3c5f64521839a7a91625
paula cobzaru <paulacobzaru@yahoo.com>
2021-01-29 00:16:33 +0000

    commit 2

--- src/main/java/com/calculator/controller/PageController.java
+++ src/main/java/com/calculator/controller/PageController.java
@@ -1,16 +1,64 @@
 package com.calculator.controller;
 
+import com.calculator.service.CalculatorImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
+    //    @Autowired
+//    private Integer x;
+//    @Autowired
+//    private Integer y;
+//    @Autowired(required = true)
+    private Integer result1;
+    private Integer result2;
+    private Integer result3;
+    private Integer result4;
+    private Integer result5;
 
-    @GetMapping("/ageCalculator")
-    public String ageCalculator(Model model){
 
-        return "/ageCalculator";
+    @GetMapping("/addCalculator")
+    public String add(@PathVariable int x, @PathVariable int y, Model model) {
+        CalculatorImpl calculator = new CalculatorImpl();
+        calculator.add(x, y);
+        model.addAttribute("Your addition result is: ", result1);
+        return "/addCalculator";
     }
 
+    @GetMapping("/subtractionCalculator")
+    public String minus(@PathVariable int x, @PathVariable int y, Model model) {
+        CalculatorImpl calculator = new CalculatorImpl();
+        calculator.minus(x, y);
+        model.addAttribute("Your substraction result is: ", result2);
+        return "/subtrationCalculator";
+    }
+
+    @GetMapping("/multiplyCalculator")
+    public String multiply(@PathVariable int x, @PathVariable int y, Model model) {
+        CalculatorImpl calculator = new CalculatorImpl();
+        calculator.multiply(x, y);
+        model.addAttribute("Your multiply result is: ", result3);
+        return "/multiplyCalculator";
+    }
+
+    @GetMapping("/divisionCalculator")
+    public String division(@PathVariable int x, @PathVariable int y, Model model) {
+        CalculatorImpl calculator = new CalculatorImpl();
+        calculator.division(x, y);
+        model.addAttribute("Your division result is: ", result4);
+        return "/divisionCalculator";
+    }
+
+    @GetMapping("/moduloCalculator")
+    public String modulo(@PathVariable int x, @PathVariable int y, Model model) {
+        CalculatorImpl calculator = new CalculatorImpl();
+        calculator.modulo(x, y);
+        model.addAttribute("Your modulo result is: ", result5);
+        return "/moduloCalculator";
+    }
 }
+
---
+++ src/main/java/com/calculator/service/Calculator.java
@@ -0,0 +1,14 @@
+package com.calculator.service;
+
+public interface Calculator {
+
+    Integer add(int x, int y);
+
+    Integer minus(int x, int y);
+
+    Integer multiply(int x, int y);
+
+    Integer division(int x, int y);
+
+    Integer modulo(int x, int y);
+}
